c58b31765bdd4d1532833d64f6ba736d91c517a0
Kevin Burleigh <klb@kindling>
2016-12-15 13:49:03 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='klb_package',
+      version='0.1',
+      description='Package experiment',
+      url='http://github.com/kevinburleigh75/klb_package',
+      author='klb',
+      author_email='noone@example.com',
+      license='MIT',
+      packages=['klb_package'],
+      zip_safe=False)
